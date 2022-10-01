999f809177f36ce8f51ce5044607ecffe22cfea5
Jaxon Desktop <handtjaxon@gmail.com>
2022-10-01 15:45:00 -0500

    Finished initial project setup

--- server/index.js
+++ server/index.js
@@ -3,3 +3,11 @@ import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://admin:admin123@cluster0.fswaxvy.mongodb.net/?retryWrites=true&w=majority";
+
